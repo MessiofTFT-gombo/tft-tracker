a991cf1d3752b80ca7afcb7e409445ac34b5170f
MessiofTFT-gombo <lhh1023@gachon.ac.kr>
2025-04-09 12:24:18 +0900

    Fix: move getTFTData to pages/api and handle fetch error

--- pages/api/getTFTData.js
+++ pages/api/getTFTData.js
@@ -7,18 +7,18 @@ export default async function handler(req, res) {
 
   try {
     const accountRes = await fetch(`https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(riotId)}/${tagLine}`, {
-      headers: { 'X-Riot-Token': RIOT_API_KEY }
+      headers: { 'X-Riot-Token': 'RGAPI-16c3c08b-132a-49a1-beb5-a1d4f5e8eaff' }
     });
     const accountData = await accountRes.json();
     const puuid = accountData.puuid;
 
     const summonerRes = await fetch(`https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/${puuid}`, {
-      headers: { 'X-Riot-Token': RIOT_API_KEY }
+      headers: { 'X-Riot-Token': 'RGAPI-16c3c08b-132a-49a1-beb5-a1d4f5e8eaff' }
     });
     const summonerId = (await summonerRes.json()).id;
 
     const leagueRes = await fetch(`https://kr.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerId}`, {
-      headers: { 'X-Riot-Token': RIOT_API_KEY }
+      headers: { 'X-Riot-Token': 'RGAPI-16c3c08b-132a-49a1-beb5-a1d4f5e8eaff' }
     });
     const leagueData = await leagueRes.json();
     const tft = leagueData.find(entry => entry.queueType === 'RANKED_TFT');
